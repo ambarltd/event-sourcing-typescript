5658378896b1492b0291071d3ec4d62f9164886f
Marcelo Lazaroni <lazaronijunior@gmail.com>
2025-08-28 16:35:31 +0100

    Add unit tests

---
+++ tests/unit/suites/example.ts
@@ -0,0 +1,12 @@
+export { tests };
+
+import { test, group, expect } from '../lib/test';
+
+const tests = group('Example test group', [
+  group('subgroup', [
+    test('should work', async () => {
+      const list = [1, 2, 3];
+      return expect.equals(list.reverse(), list.reverse().reverse());
+    }),
+  ]),
+]);
