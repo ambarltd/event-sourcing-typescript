d4459a686f626345e4371998ba6c5ff950ab752f
Marcelo Lazaroni <lazaronijunior@gmail.com>
2025-09-01 16:16:27 +0100

    Add environment parser

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -9,8 +9,14 @@ import * as D from '@/lib/json/decoder';
 
 type Environment = D.Infer<typeof envDecoder>;
 
+const string = D.string;
+const number = D.stringNumber;
+
 // All expected environment variables are checked at program initialization.
-const envDecoder = D.object({});
+const envDecoder = D.object({
+  SMTP_HOST: string,
+  SMTP_PORT: number,
+});
 
 const environment: Environment = D.decode(process.env, envDecoder).unwrap(err => {
   throw new Error(`Unable to parse environment variables:\n${err}`);
