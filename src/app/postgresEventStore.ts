32342522b502ca1219426552952d50424adfccbc
Marcelo Lazaroni <lazaronijunior@gmail.com>
2025-09-10 13:05:30 +0100

    Create initialisation script for PostgreSQL

--- src/app/postgresEventStore.ts
+++ src/app/postgresEventStore.ts
@@ -1,4 +1,4 @@
-export { PostgresEventStore };
+export { initialize, PostgresEventStore };
 
 import { Serializer } from '@/common/serializedEvent/Serializer';
 import { Deserializer } from '@/common/serializedEvent/Deserializer';
@@ -10,6 +10,7 @@ import { Aggregate } from '@/common/aggregate/Aggregate';
 import { AggregateAndEventIdsInLastEvent } from '@/common/eventStore/AggregateAndEventIdsInLastEvent';
 import { EventStore } from '@/lib/eventSourcing/eventStore';
 import { PostgresTransaction } from '@/lib/postgres';
+import { log } from '@/common/util/Logger';
 
 class PostgresEventStore implements EventStore {
   constructor(
@@ -165,4 +166,96 @@ class PostgresEventStore implements EventStore {
   ): event is TransformationEvent<T> {
     return event instanceof TransformationEvent;
   }
+}
+
+// Prepare the database to be used as an event store.
+async function initialize({
+  transaction,
+  database,
+  table,
+  replicationUserName,
+  replicationUserPass,
+  replicationPublication,
+}: {
+  transaction: PostgresTransaction;
+  database: string;
+  table: string;
+  replicationUserName: string;
+  replicationUserPass: string;
+  replicationPublication: string;
+}): Promise<void> {
+  function run(description: string, query: string) {
+    log.info(description);
+    log.info(query);
+    return transaction.query(query);
+  }
+
+  await run(
+    `Creating table ${table}`,
+    `CREATE TABLE IF NOT EXISTS ${table} (
+         id BIGSERIAL NOT NULL,
+         event_id TEXT NOT NULL UNIQUE,
+         aggregate_id TEXT NOT NULL,
+         aggregate_version BIGINT NOT NULL,
+         causation_id TEXT NOT NULL,
+         correlation_id TEXT NOT NULL,
+         recorded_on TEXT NOT NULL,
+         event_name TEXT NOT NULL,
+         json_payload TEXT NOT NULL,
+         json_metadata TEXT NOT NULL,
+         PRIMARY KEY (id)
+     );`,
+  );
+
+  await run(
+    'Creating replication user',
+    `CREATE USER ${replicationUserName} REPLICATION LOGIN PASSWORD '${replicationUserPass}';`,
+  );
+
+  await run(
+    'Granting permissions to replication user',
+    `GRANT CONNECT ON DATABASE "${database}" TO ${replicationUserName};`,
+  );
+
+  await run(
+    'Granting select to replication user',
+    `GRANT SELECT ON TABLE ${table} TO ${replicationUserName};`,
+  );
+
+  // Create publication
+  await run(
+    'Creating publication for table',
+    `CREATE PUBLICATION ${replicationPublication} FOR TABLE ${table};`,
+  );
+
+  // Create indexes
+  await run(
+    'Creating aggregate id, aggregate version index',
+    `CREATE UNIQUE INDEX event_store_idx_event_aggregate_id_version ON ${table}(aggregate_id, aggregate_version);`,
+  );
+
+  await run(
+    'Creating id index',
+    `CREATE UNIQUE INDEX event_store_idx_event_id ON ${table}(event_id);`,
+  );
+
+  await run(
+    'Creating causation index',
+    `CREATE INDEX event_store_idx_event_causation_id ON ${table}(causation_id);`,
+  );
+
+  await run(
+    'Creating correlation index',
+    `CREATE INDEX event_store_idx_event_correlation_id ON ${table}(correlation_id);`,
+  );
+
+  await run(
+    'Creating recording index',
+    `CREATE INDEX event_store_idx_occurred_on ON ${table}(recorded_on);`,
+  );
+
+  await run(
+    'Creating event name index',
+    `CREATE INDEX event_store_idx_event_name ON ${table}(event_name);`,
+  );
 }
