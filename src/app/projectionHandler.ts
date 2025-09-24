175b66a7e2d401cd864da935fc2c6bce139c2606
Marcelo Lazaroni <lazaronijunior@gmail.com>
2025-09-24 19:53:40 +0100

    Implement projection handler

---
+++ src/app/projectionHandler.ts
@@ -0,0 +1,80 @@
+export { handleProjection };
+
+import { Response } from '@/lib/router';
+import { Event } from '@/lib/eventSourcing/event';
+import { Decoder, decode } from '@/lib/json/decoder';
+import * as express from 'express';
+import * as router from '@/lib/router';
+import * as d from '@/lib/json/decoder';
+import { Future } from '@/lib/Future';
+import { Result, Failure } from '@/lib/Result';
+import { Maybe, Nothing } from '@/lib/Maybe';
+
+type Projections = {};
+type ProjectionStore = {};
+type Mongo = {};
+
+type ProjectionController<E extends Event<any>> = {
+  decoder: Decoder<Maybe<E>>;
+  handler: (v: {
+    event: E;
+    projections: Projections;
+    store: ProjectionStore;
+  }) => Future<Response, Response>;
+};
+
+function handleProjection<E extends Event<any>>(
+  projections: Projections,
+  mongo: Mongo,
+  { decoder, handler }: ProjectionController<E>,
+): express.Handler {
+  return router.route((req) =>
+    decodeEvent(decoder, req).chain((event) =>
+      withProjectionStore(mongo, (store) =>
+        handler({
+          event,
+          projections,
+          store,
+        }),
+      ),
+    ),
+  );
+}
+
+function decodeEvent<E>(
+  decoder: Decoder<Maybe<E>>,
+  req: express.Request,
+): Future<Response, E> {
+  const bodyDecoder: Decoder<Maybe<E>> = d
+    .object({ payload: decoder })
+    .map((r) => r.payload);
+
+  const decoded: Result<string, Maybe<E>> = decode(bodyDecoder, req.body);
+
+  if (decoded instanceof Failure) {
+    return Future.reject(
+      router.json({
+        status: 400,
+        content: { message: `Unable to decode command: ${decoded.error}` },
+      }),
+    );
+  }
+
+  if (decoded.value instanceof Nothing) {
+    return Future.reject(
+      router.json({
+        status: 200,
+        content: { message: 'Ignored' },
+      }),
+    );
+  }
+
+  return Future.resolve(decoded.value.value);
+}
+
+function withProjectionStore(
+  _mongo: Mongo,
+  _f: (s: ProjectionStore) => Future<Response, Response>,
+): Future<Response, Response> {
+  throw new Error('TODO');
+}
