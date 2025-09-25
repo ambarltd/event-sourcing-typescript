315128714afc621ccd84961668a0e6a32d1cb424
rafaeelricco <rafaelricco10@gmail.com>
2025-09-25 16:11:29 -0300

    renamed local-development to development

--- local-development/podman-scripts/windows/dev_shutdown.ps1
+++
@@ -1,9 +0,0 @@
-$ErrorActionPreference = 'Stop'
-$originalLocation = Get-Location
-
-try {
-    Set-Location -Path (Split-Path (Split-Path $PSScriptRoot -Parent) -Parent)
-    podman compose down
-} finally {
-    Set-Location -Path $originalLocation
-}
